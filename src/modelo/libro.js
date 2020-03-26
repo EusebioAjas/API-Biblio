9fcfbe2d2c9d267063b85f0476f5a82bc9e2f551
EusebioAjas <eusebioajas6@gmail.com>
2020-03-26 14:04:30 -0600

    atributos completos

--- src/modelo/libro.js
+++ src/modelo/libro.js
@@ -4,7 +4,20 @@ const libroSchema = new Schema({
     isbn:{
         type: String,
         required:true
+    },
+    autor:{
+        type: String,
+        requiered:true
+    },
+    numero_paginas:{
+        type: Number,
+        required: true
+    },
+    nombre_libro:{
+        type: String,
+        required: true
     }
+
 });
 
 module.exports = model('libro',libroSchema);
