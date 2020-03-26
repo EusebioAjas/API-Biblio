0e91d74c8c96600d42e2f58a5b6b2d44796826d2
EusebioAjas <eusebioajas6@gmail.com>
2020-03-26 16:36:12 -0600

    Método Obtener

--- src/controlador/libro.js
+++ src/controlador/libro.js
@@ -12,9 +12,19 @@ libroCtrl.crear = async (req, res) => {
     });
     await nuevoLibro.save();
     res.json({ nuevoLibro });
+    console.log(nuevoLibro);
   } catch (error) {
       res.json({message: error});
   }
+};
+
+libroCtrl.obtener = async (req, res) => {
+    try{
+        const libros = await Libro.find(); 
+        res.json({libros});
+    }catch(err){
+        res.json({message:err});
+    }
 };
 
 module.exports = libroCtrl;
--- src/rutas/libro.js
+++ src/rutas/libro.js
@@ -1,7 +1,8 @@
 const {Router} = require('express');
 const router = Router();
-const {crear} = require('../controlador/libro');
+const {crear, obtener} = require('../controlador/libro');
 
 router.post('/libros',crear);
+router.get('/libros', obtener);
 
 module.exports = router;
