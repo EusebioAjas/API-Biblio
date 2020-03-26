74fec78f27eef70bdf62519d8bfefab1fea4671c
EusebioAjas <eusebioajas6@gmail.com>
2020-03-26 14:14:23 -0600

    body-parser

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
 require('./bd');
+
+const cors = require('cors');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cors());
 
 app.set('port',3000);
 app.listen(app.get('port'),()=>{
